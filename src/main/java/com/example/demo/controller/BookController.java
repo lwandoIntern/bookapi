9176243f3a9c381fd4f18ca87cd7585d7cde66a1
lwandoIntern <ncebagoniwe@gmail.com>
2020-01-15 12:00:59 +0200

    minor controller issue fixing

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -3,13 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.model.Book;
 import com.example.demo.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/boookapi")
+@RestController("/bookapi")
 public class BookController {
 
     @Autowired
